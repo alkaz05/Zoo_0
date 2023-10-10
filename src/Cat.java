690c3d90474f69f6c32ea120c439e251ba2da40f
RORUHEUGEN <livingworthilydiefree@mail.com>
2023-10-10 13:35:19 +0300

    добавил класс Cat добавил конструктор добавил приветствие

--- src/Cat.java
+++ src/Cat.java
@@ -1,6 +1,10 @@
 public class Cat {
     String name;
     int age;
+    public Cat(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
 
     public static void hello(String name, int age){
         System.out.println("Hello i'm " + name + " и мне " + age);
