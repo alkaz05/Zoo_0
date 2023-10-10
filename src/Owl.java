94951a27fd56dbdecad6293f2431b92ec8ae3b88
Olga Suv <suvolga08@rambler.ru>
2023-10-10 22:49:04 +0400

    plus func Owl

--- src/Owl.java
+++ src/Owl.java
@@ -4,4 +4,18 @@ public class Owl {
     void woohoo () {
         System.out.println(name + ": ухууу~");}
 
+
+    void flight(){
+        double flyHight;
+
+        if (flyHight <4 && flyHight > 0)
+            System.out.println("Сова врезалась в слона");
+        else if (flyHight = 0)
+            System.out.println("Сова решила прогуляться");
+        else if (flyHight < 0)
+            System.out.println("Сова не крот, под землёй не живёт!");
+        else System.out.println("Сова летит");
+    }
+
+}
 }
