1a7f702c8d921755642bbb92903c745520837d82
korenkovaelena <korenkovalena@gmail.com>
2023-10-10 13:12:46 +0300

    Добавлен Ворон

---
+++ src/Raven.java
@@ -0,0 +1,10 @@
+public class Raven {public static void main(String[] args) {
+    System.out.println("Raven Elena");
+    talk();
+}
+public static void talk(){
+    System.out.println("Ворон говорит \"Кар!\"");
+}
+}
+
+
