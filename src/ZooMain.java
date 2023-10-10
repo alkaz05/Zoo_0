d9fcf9465b844c95cb46ee11a37fe31bf6b0f5ee
Aleksandr K <alkaz05@mail.ru>
2023-10-10 13:10:08 +0300

    +

--- src/ZooMain.java
+++ src/ZooMain.java
@@ -1,5 +1,7 @@
 public class ZooMain {
     public static void main(String[] args) {
         System.out.println("Добро пожаловать в наш Зоопарк");
+
+        Сrocodile crok = new Сrocodile('a','b', 'c' );
     }
 }
