9cdff1ecc23b448a62fcf3277a3562e0daa49d5a
RORUHEUGEN <livingworthilydiefree@mail.com>
2023-10-11 10:50:58 +0300

    добавил класс Cat добавил конструктор добавил приветствие добавил комит elfkbk ; add main add get & set

--- src/Cat.java
+++ src/Cat.java
@@ -1,12 +1,22 @@
 public class Cat {
     public static void main(String[] args) {
-        String name;
-        int age;
+        privat String name;
+        privat int age;
     //public Cat(String name, int age){
        //     this.name = name;
        //     this.age = age;
         }
+        public static void setIt(String name, int age){
 
+        }
+
+        public static String getName(){
+        return this.name;
+        }
+
+        public static String getAge(){
+        return this.age;
+    }
         public static void hello(String name, int age){
             System.out.println("Hello i'm " + name + " и мне " + age);
         }
