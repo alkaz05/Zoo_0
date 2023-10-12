ca3cf8a9ee07eb163353d8dd7119db84e84b6491
olesi4ka777 <olesi4ka777@gmail.com>
2023-10-12 23:35:47 +0300

    Добавлен хомяк,которого скоро съест кошка

---
+++ src/Humster.java
@@ -0,0 +1,12 @@
+public class Hamster {
+    public static void main(String[] args){
+        System.out.println("Humster likes to eat");
+        //характер хомячка
+        String name="Hamster name's Fat";
+        //имя хомячка
+        String sound="om-nom-nom";
+        System.out.println(name);
+        System.out.println("Он снова что-то ест!Слышите? "+sound);
+        System.out.println ("Но вдруг появилась кошка");
+    }
+}
